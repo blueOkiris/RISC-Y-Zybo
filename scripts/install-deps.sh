8eff68f962db80c061d78c42d9abb4a5fd82d0f1
Dylan Turner <dylantdmt@gmail.com>
2021-02-13 00:19:49 -0500

    Update scripts a bit

---
+++ scripts/install-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install sbt
+echo "Installing sbt..."
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt update
+sudo apt -y install sbt
+echo "Done."
+
+echo "Installing java and scala..."
+sudo apt install -y openjdk-8-jdk scala
